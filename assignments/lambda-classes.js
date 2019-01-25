8aea7e874ce72410e4328fb5dd06f518fcab552c
Reyaad Rafeek <reyaad510@gmail.com>
2019-01-24 22:18:39 -0500

    Completed all MVP goals

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -84,3 +84,106 @@ class ProjectManager extends Instructor {
       return `${this.name} debugs ${student.name}'s code on ${subject}.`
     }
 }
+
+
+// Instructors 
+
+const fred = new Instructor ({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'Male',
+    favLanguage: 'Javascript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+})
+
+const scooby = new Instructor ({
+    name: 'Scooby',
+    location: 'Africa',
+    age: 65,
+    gender: 'Female',
+    favLanguage: 'HTML',
+    specialty: 'Front-end',
+    catchPhrase: `Scooby dooby doooo!`
+})
+
+
+
+// Students
+
+const reyaad = new Student ({
+    name: 'Reyaad',
+    location: 'Leesburg',
+    age: 24,
+    gender: 'Male',
+    previousBackground: 'Biology',
+    className: 'BS101',
+    favSubjects: ['HTML', 'CSS', 'JavaScript']
+})
+
+const shaggy = new Student ({
+    name: 'Shaggy',
+    location: 'Antartica',
+    age: 89,
+    gender: 'Male',
+    previousBackground: 'Ghostology',
+    className: 'GH101',
+    favSubjects: ['Ghosts for Beginners', 'Scooby Snacks', `How To Treat Dogs`]
+})
+
+
+
+// Project Managers
+
+const joe = new ProjectManager ({
+    name: 'Joe',
+    location: 'Cali',
+    age: 28,
+    gender: 'Male',
+    favLanguage: 'React',
+    specialty: 'Front-end',
+    catchPhrase: `Give me that 50k or more paying job!`,
+    gradClassName: 'CS1',
+    favInstructor: 'Cameron Pope'
+})
+
+const zombie = new ProjectManager ({
+    name: 'Zombie',
+    location: 'Graveyard',
+    age: 198,
+    gender: 'Male',
+    favLanguage: 'Gyarhhh',
+    specialty: 'Eating Flesh',
+    catchPhrase: `Gyarrrrhhhhhhhh!!`,
+    gradClassName: 'ZombiesRCool',
+    favInstructor: 'Mr.Ghoul'
+})
+
+
+// Instructor
+console.log(fred);
+console.log(fred.demo('Math'));
+console.log(fred.grade(reyaad, 'Calc'));
+console.log(scooby.catchPhrase);
+console.log(scooby.favLanguage);
+console.log(scooby.specialty);
+
+// Student
+console.log(reyaad);
+console.log(reyaad.listsSubjects());
+console.log(reyaad.sprintChallenge('JavaScript'));
+console.log(shaggy.age);
+console.log(shaggy.gender);
+console.log(shaggy.className);
+console.log(shaggy.location);
+console.log(shaggy.favSubjects);
+
+
+// Project Manager
+console.log(joe);
+console.log(joe.standUp('webpt4'));
+console.log(joe.debugsCode(reyaad, 'React'));
+console.log(zombie.catchPhrase);
+console.log(zombie.gradClassName);
+console.log(zombie.favInstructor);
